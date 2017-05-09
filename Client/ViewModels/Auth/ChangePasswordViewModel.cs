d60b0ad092ba0b0ee61502135c417008060d70c8
Dzmitry Kastsiukevich <dmitry.kostukevich@gmail.com>
2017-05-09 13:21:25 +0300

    Minor changes in change model

--- Client/ViewModels/Auth/ChangePasswordViewModel.cs
+++ Client/ViewModels/Auth/ChangePasswordViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Client.ViewModel.Auth
+namespace Client.ViewModels.Auth
 {
     public class ChangePasswordViewModel
     {
