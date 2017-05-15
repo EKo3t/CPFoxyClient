06baece14c2528e365ecce5dc49d4111d048e282
haritonikk96 <haritonikk96@gmail.ru>
2017-05-15 04:41:10 +0300

    перевод

--- Client/ViewModels/Auth/RegisterViewModel.cs
+++ Client/ViewModels/Auth/RegisterViewModel.cs
@@ -10,26 +10,31 @@ namespace Client.ViewModels.Auth
     {
         [Required(ErrorMessage = "Введите непустую почту")]
         [StringLength(100, ErrorMessage = "Слишком длинная почта", MinimumLength = 5)]
-        [Display(Name = "Email")]
+        [Display(Name = "Почта")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Введите непустой пароль")]
         [StringLength(100, ErrorMessage = "Слишком короткий пароль", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Введите непустое подтверждение пароля")]
         [DataType(DataType.Password)]
         [StringLength(100, ErrorMessage = "Подтверждение пароля слишком короткое", MinimumLength = 6)]
         [Compare("Password", ErrorMessage = "Пароли не совпадают.")]
+        [Display(Name = "Повторите пароль")]
         public string ConfirmPassword { get; set; }
 
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
+        [Display(Name = "Отчество")]
         public string MiddleName { get; set; }
 
         [DataType(DataType.Date, ErrorMessage = "Введите дату по формату")]
+        [Display(Name = "Дата рождения")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd.MM.yyyy}")]
         public DateTime BirthDate { get; set; }
     }
--- Client/ViewModels/Order/OrderViewModel.cs
+++ Client/ViewModels/Order/OrderViewModel.cs
@@ -12,17 +12,21 @@ namespace Client.ViewModels.Order
         public Guid Id { get; set; }
 
         [Required]
+        [Display(Name = "Время отправления")]
         [DataType(DataType.Date, ErrorMessage = "Введите дату по формату")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd.MM.yyyy HH:mm}")]
         public DateTime OrderTime { get; set; }
 
         [Required]
+        [Display(Name = "Место отправления")]
         public string StartAddress { get; set; }
 
+        [Display(Name = "Место прибытия")]
         public string EndAddress { get; set; }
 
         public OrderStatus Status { get; set; }
 
+        [Display(Name = "Услуги")]
         public ServiceVM Service { get; set; }
 
         public Guid ServiceId { get; set; }
